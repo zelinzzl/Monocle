32978e32b02f76dd4d500922c0af7c080f89a7fa
Ze-Lin Zhang <111583652+zelinzzl@users.noreply.github.com>
2025-07-29 23:42:52 +0200

    fixed public directory

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: "Monocle",
   description: "",
   icons: {
-    icon: "/Logo-light.svg",
+    icon: "/Logo.svg",
   },
 };
 
@@ -27,10 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${roboto.variable} antialiased`}>
         <AuthProvider>
-          
-           <ThemeProvider> 
-               {children}
-             </ThemeProvider> 
+          <ThemeProvider>{children}</ThemeProvider>
         </AuthProvider>
       </body>
     </html>
