d7aa6d4c5528cf4dadd64853556e61ed407d720b
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2025-07-29 10:54:17 +0200

    frontend tests setup

---
+++ frontend/cypress/e2e/integration-tests/login.cy.ts
@@ -0,0 +1,36 @@
+
+describe('Login page - Integration', () => {
+  let appUrl = "http://localhost:3000";
+  
+  it('Valid user', () => {
+    cy.visit(appUrl+'/signup');
+    cy.get('#firstName').type('TestName');
+    cy.get('#lastName').type('TestSurname');
+
+    cy.get('#email').type('test@example.com');
+    cy.get('#password').type('password123');
+    cy.get('#terms').click();
+
+    cy.get('button[type="submit"]').click();
+
+    //cy.url().should('include', '/dashboard');
+    //cy.contains('Welcome, TestName');
+  });
+  
+    
+  it('Invalid user', () => {
+    cy.visit(appUrl+'/signup');
+    cy.get('#firstName').type('TestName');
+    cy.get('#lastName').type('TestSurname');
+
+    cy.get('#email').type('test@example.com');
+    cy.get('#password').type('IncorrectPassword');
+    cy.get('#terms').click();
+
+    cy.get('button[type="submit"]').click();
+
+    //cy.url().should('include', '/dashboard');
+    //cy.contains('Welcome, TestName');
+  });
+
+});
