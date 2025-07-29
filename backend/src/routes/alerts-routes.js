b1b78c8f4e142d49a00751e12d559904dbf00800
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2025-07-29 22:00:32 +0200

    Alerts endpoints updated

--- backend/src/routes/alerts-routes.js
+++ backend/src/routes/alerts-routes.js
@@ -3,15 +3,15 @@ import AlertController from '../controllers/alertsController.js';
 
 const router = express.Router();
 
-router.get('/:userId', AlertController.getAlerts);
+router.get('/get-alerts/:userId', AlertController.getAlerts);
 
 // Route to delete an alert
-router.delete('/:alertId', AlertController.deleteAlertController);
+router.delete('/remove/:alertId', AlertController.deleteAlertController);
 
 // Route to update an alert's status
-router.put('/:alertId', AlertController.updateAlertStatusController);
+router.put('/update/:alertId', AlertController.updateAlertStatusController);
 
 // Route to create a new alert
-router.post('/', AlertController.createAlertController);
+router.post('/create-alert', AlertController.createAlertController);
 
 export default router;
